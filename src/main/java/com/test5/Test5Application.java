068075a7f2520aaf213ebb90e5963ed042e01a10
kashifsohail.beg@gmail.com <kashifsohail.beg@gmail.com>
2025-10-20 00:19:20 +0530

    Revert "m1"
    This reverts commit 76ab9a07088251b4a5767a9497e920f751ed7761.

--- src/main/java/com/test5/Test5Application.java
+++ src/main/java/com/test5/Test5Application.java
@@ -14,7 +14,8 @@ public class Test5Application {
 	double z =10.3;
 	boolean isPresent=false;
 	float m=10.3f;
-	char i ='n';
+	char ch ='x';
+	boolean isSub=true
 	SpringApplication.run(Test5Application.class, args);
 	}
 
